985b452735ea0baff754b251a4c689bdb2a77daa
iasoloviev61 <iasoloviev61@gmail.com>
2018-06-08 11:02:31 +0300

    create frontend template from nodejs:9 and gulp4

---
+++ appdata/cmd.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#apt-get update
+#apt-get install bzip2
+alias gulp="/usr/local/bin/gulp4"
+cd /appdata
+#npm install
+#npm audit fix
+gulp
---
+++ appdata/gulp/tasks/sass.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = function() {
+  $.gulp.task('sass', function() {
+    return $.gulp.src('./source/style/main.scss')
+      .pipe($.gp.sourcemaps.init())
+      .pipe($.gp.sass()).on('error', $.gp.notify.onError({ title: 'Style' }))
+      .pipe($.gp.autoprefixer({ browsers: $.config.autoprefixerConfig }))
+      .pipe($.gp.sourcemaps.write())
+      .pipe($.gulp.dest($.config.root + '/assets/css'))
+      // .pipe($.gp.csso())
+      // .pipe($.gp.rename("main.min.css"))
+      // .pipe($.gulp.dest($.config.root + '/assets/css'))
+      .pipe($.browserSync.stream());
+  })
+};
